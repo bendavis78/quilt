4352fa103a66c1bf62ef960ad3f14123f65d84ca
Ben Davis <bendavis78@gmail.com>
2012-04-30 17:15:43 -0500

    virtualenv: changed "no-site-pacakges" to "system-site-packages" to reflect lastest virtualenv

--- quilt/contrib/virtualenv/resources.py
+++ quilt/contrib/virtualenv/resources.py
@@ -1,11 +1,11 @@
 from quilt.contrib import fs
 from fabric.api import run, env, settings, abort, hide
 
-env.resources.virtualenv.virtualenv.no_site_packages = True
+env.resources.virtualenv.virtualenv.system_site_packages = False
 
 class VirtualEnv(fs.Directory):
     python = None
-    no_site_packages = None
+    system_site_packages = None
     
     def ensure(self):
         parent = fs.Directory(self.path.rsplit('/', 1)[0])
@@ -18,8 +18,9 @@ class VirtualEnv(fs.Directory):
                 self.python = '`which python`'
             
             self.log('Creating virtualenv at {}'.format(self.path))
-            no_site_pkgs = self.no_site_packages and ' --no-site-packages' or ''
-            cmd = 'virtualenv{} -p {} {}'.format(no_site_pkgs, self.python, self.path)
+            site_pkgs = ' --{}-site-packages'.format(
+                    self.system_site_packages and 'system' or 'no')
+            cmd = 'virtualenv{} -p {} {}'.format(site_pkgs, self.python, self.path)
             if not env.dry_run:
                 output_ctrl = settings(hide('stdout'), warn_only=True)
                 with output_ctrl, parent.temp_ownership(recursive=True):
