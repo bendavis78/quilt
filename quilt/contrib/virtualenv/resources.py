8ad3fc716aab33dd9896ae850e78390d3734dde8
Ben Davis <bendavis78@gmail.com>
2012-05-10 17:58:58 -0500

    virtualenv: added support for virtualenv versions less than 1.7

--- quilt/contrib/virtualenv/resources.py
+++ quilt/contrib/virtualenv/resources.py
@@ -18,9 +18,20 @@ class VirtualEnv(fs.Directory):
                 self.python = '`which python`'
             
             self.log('Creating virtualenv at {}'.format(self.path))
-            site_pkgs = ' --{}-site-packages'.format(
-                    self.system_site_packages and 'system' or 'no')
-            cmd = 'virtualenv{} -p {} {}'.format(site_pkgs, self.python, self.path)
+
+            # Get virtualenv version
+            with hide('stdout'):
+                version = run('virtualenv --version')
+            version = version.split('.')
+            venv_args = ''
+            if version < (1,7):
+                if not self.system_site_packages:
+                    venv_args = ' --no-site-packages'
+            else:
+                venv_args = ' --{}-site-packages'.format(
+                        self.system_site_packages and 'system' or 'no')
+
+            cmd = 'virtualenv{} -p {} {}'.format(venv_args, self.python, self.path)
             if not env.dry_run:
                 output_ctrl = settings(hide('stdout'), warn_only=True)
                 with output_ctrl, parent.temp_ownership(recursive=True):
